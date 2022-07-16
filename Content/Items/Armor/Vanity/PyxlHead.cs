9a2acfb7fa3589ffacea50da29c7b0817e5eef7c
TorchTheDragon <1thatdragonboi@gmail.com>
2022-07-16 19:42:27 -0400

    Added Pyxl Vanity Code

---
+++ Content/Items/Armor/Vanity/PyxlHead.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Overthrown.Content.Rarities;
+
+namespace Overthrown.Content.Items.Armor.Vanity
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class PyxlHead :ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Pyxl's Hait");
+			Tooltip.SetDefault("Beta Item");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 22;
+			Item.height = 20;
+
+			Item.value = Item.sellPrice(gold: 1);
+			Item.rare = ModContent.RarityType<PyxlHigherRarity>();
+			Item.vanity = true;
+			Item.maxStack = 1;
+		}
+	}
+}
