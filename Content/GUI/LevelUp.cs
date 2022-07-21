ffce8397d19bffea32eadc2eac101c2670a895f5
TorchTheDragon <1thatdragonboi@gmail.com>
2022-07-20 22:40:20 -0400

    Bruh Fix

--- Content/GUI/LevelUp.cs
+++ Content/GUI/LevelUp.cs
@@ -9,7 +9,7 @@ namespace Egoteric.Content.GUI
 {
 	internal class LevelUp : UIState
 	{
-		private static bool Visible;
+		public static bool Visible;
 
 		private UIPanel levelBackground;
 		private UIPanel separator1;
